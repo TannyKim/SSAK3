7d30eb7eae54fc13e0b75cacf0af554370018b40
TannyKim <taekyun.contact@gmail.com>
2023-12-15 08:55:42 +0900

    "main to springbootapplication"

---
+++ BE/src/main/java/org/ssak3/Ssack3Application.java
@@ -0,0 +1,12 @@
+package org.ssak3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssack3Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        SpringApplication.run(Ssack3Application.class, args);
+    }
+}
