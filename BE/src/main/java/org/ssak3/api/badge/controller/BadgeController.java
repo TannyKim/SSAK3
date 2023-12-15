cb24e069151ed24ff3ffbeb5e4e7078b6994659f
cade <chyeon81@icloud.com>
2023-12-15 10:29:15 +0900

    Setting up Swagger UI

--- BE/src/main/java/org/ssak3/api/badge/controller/BadgeController.java
+++ BE/src/main/java/org/ssak3/api/badge/controller/BadgeController.java
@@ -5,11 +5,17 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.ssak3.api.badge.entity.OriginBadge;
 import org.ssak3.api.badge.service.BadgeService;
+
+import java.util.List;
 
 @Tag(name = "TEST REST API", description = "TEST REST API입니다.")
 @Slf4j
@@ -18,14 +24,33 @@ import org.ssak3.api.badge.service.BadgeService;
 @RequestMapping("/api/v1/ledger")
 public class BadgeController {
 
+    @Autowired
     private final BadgeService badgeService = new BadgeService();
 
     @Operation(summary = "문자열 반복", description = "파라미터로 받은 문자열을 2번 반복합니다.")
     @Parameter(name = "str", description = "2번 반복할 문자열")
     @GetMapping("/returnStr")
-    public String returnStr(@RequestParam String str) {
+    public String returnStr(@RequestParam("id") String str) {
         return str + "\n" + str;
     }
+
+    @Operation(summary = "배지 조회", description = "배지 목록을 조회합니다.")
+    @GetMapping("/badge")
+    public ResponseEntity<List<OriginBadge>> originBadgeList() {
+
+        List<OriginBadge> list;
+        try {
+            log.info("start select badge list");
+            list = badgeService.findAllBadgeList();
+        } catch (Exception e) {
+            log.error("error", e);
+            throw new RuntimeException(e);
+        }
+        log.info("badge counts: " + list.size());
+
+        return new ResponseEntity<List<OriginBadge>>(list, HttpStatus.OK);
+    }
+
 }
 
 //@Tag(name = "BADGE REST API", description = "BADGE REST API입니다.")
