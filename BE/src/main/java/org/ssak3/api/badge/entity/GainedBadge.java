81748d4b63e33e437960b73a007f681ffd2208e4
tanny <taekyun.contact@gmail.com>
2023-12-16 00:14:01 +0900

    "CustomCategory entity modified"

--- BE/src/main/java/org/ssak3/api/badge/entity/GainedBadge.java
+++ BE/src/main/java/org/ssak3/api/badge/entity/GainedBadge.java
@@ -2,19 +2,27 @@ package org.ssak3.api.badge.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.ssak3.api.user.entity.User;
 
 @Entity
 @Table(name="GAINED_BADGE")
 @Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GainedBadge {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="GAINED_BADGE_ID", columnDefinition="BIGINT UNSIGNED")
     private Long gainedBadgeId; // 획득 배지 아이디
 
-    @Column(name="BADGE_NAME", columnDefinition = "VARCHAR(50)")
-    private String badgeName; // 배지명
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "ORIGIN_BADGE_ID")
+    private OriginBadge originBadge;
+
+    @Column(name="GAINED_BADGE_NAME", columnDefinition = "VARCHAR(50)")
+    private String gainedBadgeName; // 배지명
 }
