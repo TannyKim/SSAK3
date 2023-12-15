6d8a9066b82cd83e2cfc0c3d8b571e33aa9b81a0
tanny <taekyun.contact@gmail.com>
2023-12-16 00:17:40 +0900

    "origin category entity added"

--- BE/src/main/java/org/ssak3/api/category/entity/OriginCategory.java
+++ BE/src/main/java/org/ssak3/api/category/entity/OriginCategory.java
@@ -1,4 +1,23 @@
 package org.ssak3.api.category.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.ssak3.api.ledger.entity.Theme;
+
+@Entity
+@Table(name="ORIGIN_CATEGORY")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OriginCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ORIGIN_CATEGORY_ID", columnDefinition="BIGINT UNSIGNED")
+    private Long originCategoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "THEME_ID")
+    private Theme theme;
 }
