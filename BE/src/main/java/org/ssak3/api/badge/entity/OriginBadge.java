dbf6dd3a659ce9a4a8ecb2144f1d324a2e5b3083
tanny <taekyun.contact@gmail.com>
2023-12-16 00:17:25 +0900

    "origin badge entity relation added"

--- BE/src/main/java/org/ssak3/api/badge/entity/OriginBadge.java
+++ BE/src/main/java/org/ssak3/api/badge/entity/OriginBadge.java
@@ -6,15 +6,14 @@ import lombok.*;
 @Entity
 @Table(name="ORIGIN_BADGE")
 @Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OriginBadge {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="ORIGIN_BADGE_ID", columnDefinition="BIGINT UNSIGNED")
     private Long originBadgeId; // 배지 아이디
 
-    @Column(name="BADGE_NAME", columnDefinition = "VARCHAR(50)")
+    @Column(name="ORIGIN_BADGE_NAME", columnDefinition = "VARCHAR(50)")
     private String badgeName; // 배지명
 }
