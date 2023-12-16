e3e12d82aa0535eeb0bebc4640e828ba8ab26fd6
tanny <taekyun.contact@gmail.com>
2023-12-16 19:58:34 +0900

    "usercontroller edit"

--- BE/src/main/java/org/ssak3/api/user/controller/UserController.java
+++ BE/src/main/java/org/ssak3/api/user/controller/UserController.java
@@ -1,45 +1,59 @@
 package org.ssak3.api.user.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.ssak3.api.badge.entity.OriginBadge;
+import org.ssak3.api.user.dto.response.UserResponse;
 import org.ssak3.api.user.entity.User;
 import org.ssak3.api.user.service.UserService;
 
-import java.util.List;
-
 @Tag(name="User REST API", description = "User REST API입니다.")
-@Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/ledger")
+@Validated
+@RequestMapping("/api/v1/user")
 public class UserController {
 
-    @Autowired
     private final UserService userService;
 
     @Operation(summary = "유저 조회", description = "유저를 조회합니다.")
-    @GetMapping("/user")
-    public ResponseEntity<User> userById(@Parameter(name="userId") Long userId) {
-
-        User user;
-        try {
-            log.info("start select user by id");
-            user = userService.findUserById(userId);
-        } catch (Exception e) {
-            log.error("error", e);
-            throw new RuntimeException(e);
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "유저 조회 성공", content = @Content(schema = @Schema(implementation = UserResponse.class))),
+            @ApiResponse(responseCode = "404", description = "유저 조회 실패")
+    })
+    @PostMapping("")
+    public ResponseEntity<?> userById(Long userId) throws Exception {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw (new Exception("유저 아이디를 확인해주세요"));
         }
+        UserResponse userResponse = new UserResponse(user);
+        return ResponseEntity.status(200).body(userResponse);
+    }
 
-        return new ResponseEntity<User>(user, HttpStatus.OK);
+    @Operation(summary = "유저 생성", description = "유저를 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "유저 생성 성공", content = @Content(schema = @Schema(implementation = UserResponse.class))),
+            @ApiResponse(responseCode = "404", description = "유저 생성 실패")
+    })
+    @PostMapping("/create")
+    public ResponseEntity<?> userAdd(@Valid @RequestBody boolean isNewUser) throws Exception {
+        if (!isNewUser) {
+            throw (new Exception("등록된 유저인지 확인해주세요"));
+        }
+        User newUser = userService.insertUser();
+        UserResponse userResponse = new UserResponse(newUser);
+        return ResponseEntity.status(200).body(userResponse);
     }
 }
