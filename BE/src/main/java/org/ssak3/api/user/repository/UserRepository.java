94a5097d8a79fdab9bc0afe0195b60a7a699558f
tanny <taekyun.contact@gmail.com>
2023-12-16 19:58:48 +0900

    "userrepository edit"

--- BE/src/main/java/org/ssak3/api/user/repository/UserRepository.java
+++ BE/src/main/java/org/ssak3/api/user/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package org.ssak3.api.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.ssak3.api.user.entity.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
