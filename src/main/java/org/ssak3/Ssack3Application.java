6b0c419de699ea0a0814a6f56decbd8d0593d587
TannyKim <taekyun.contact@gmail.com>
2023-12-18 09:53:21 +0900

    "jenkins check"

--- src/main/java/org/ssak3/Ssack3Application.java
+++ src/main/java/org/ssak3/Ssack3Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ssack3Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello jenkins");
         SpringApplication.run(Ssack3Application.class, args);
     }
 }
