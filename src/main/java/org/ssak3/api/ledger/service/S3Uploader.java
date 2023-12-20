16fe4ea749bc680a4401d674c773917b1ba0d400
TannyKim <taekyun.contact@gmail.com>
2023-12-20 15:22:15 +0900

    "trim import"

--- src/main/java/org/ssak3/api/ledger/service/S3Uploader.java
+++ src/main/java/org/ssak3/api/ledger/service/S3Uploader.java
@@ -3,7 +3,6 @@ package org.ssak3.api.ledger.service;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
